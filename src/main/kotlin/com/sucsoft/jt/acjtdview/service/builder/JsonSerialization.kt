e5e5e126d5357d4735defda4ae798489316a463d
SHENLQ2018\shenlq <shenlq@sucsoft.com>
2019-03-31 17:24:59 +0800

    Revert "v1.0"
    This reverts commit 87093970b6168a6b236158ccfb2f3dd39f65989b.

--- src/main/kotlin/com/sucsoft/jt/acjtdview/service/builder/JsonSerialization.kt
+++
@@ -1,9 +0,0 @@
-package com.sucsoft.jt.acjtdview.service.builder
-
-interface JsonSerialization {
-    fun <T> deserialize(var1: String, var2: Class<T>): T
-
-    fun serialize(var1: Any): String
-
-    fun <T> deserializeList(var1: List<Any>, var2: Class<T>): List<T>
-}
